3232404c4780b48f415403c402c5af658095fa0a
James Hall <james.hall01@outlook.com>
2024-02-19 22:17:13 +0000

    Hide other players in Velvet Room

--- src/main/java/mc/duzo/persona/mixin/client/PlayerEntityRendererMixin.java
+++ src/main/java/mc/duzo/persona/mixin/client/PlayerEntityRendererMixin.java
@@ -2,12 +2,16 @@ package mc.duzo.persona.mixin.client;
 
 import mc.duzo.persona.client.feature.MaskFeatureRenderer;
 import mc.duzo.persona.client.feature.PersonaFeatureRenderer;
+import mc.duzo.persona.util.VelvetUtil;
 import net.fabricmc.loader.api.metadata.Person;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,5 +29,12 @@ public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<Abs
 
         this.addFeature(new PersonaFeatureRenderer<>(renderer, ctx.getModelLoader()));
         this.addFeature(new MaskFeatureRenderer<>(renderer, ctx.getModelLoader()));
+    }
+
+    @Inject(method = "render(Lnet/minecraft/client/network/AbstractClientPlayerEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"), cancellable = true)
+    public void persona$renderPlayer(AbstractClientPlayerEntity player, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        if (!player.equals(MinecraftClient.getInstance().player) && VelvetUtil.isInVelvetRoom(player)) {
+            ci.cancel();
+        }
     }
 }
