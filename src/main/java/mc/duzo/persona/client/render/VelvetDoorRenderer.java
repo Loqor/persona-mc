293b524ba272f97b60c099af64308d3821c8d035
James Hall <james.hall01@outlook.com>
2024-02-20 19:10:54 +0000

    Abstract render method -> Moved render code into override in P4DoorModel

--- src/main/java/mc/duzo/persona/client/render/VelvetDoorRenderer.java
+++ src/main/java/mc/duzo/persona/client/render/VelvetDoorRenderer.java
@@ -24,23 +24,7 @@ public class VelvetDoorRenderer extends EntityRenderer<VelvetDoorEntity> {
 
     @Override
     public void render(VelvetDoorEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
-        matrices.push();
-        matrices.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(180f));
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(entity.getBodyYaw()));
-
-        // todo move this somewhere better eg into P4DoorModel
-        this.model.getChild("end_portal").ifPresent(cube -> {
-            if(MinecraftClient.getInstance().player.distanceTo(entity) < 2f) {
-                matrices.push();
-                matrices.scale(1f, 1f, 2f);
-                cube.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEndGateway()), 0xFF00F0, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
-                matrices.pop();
-            }
-        });
-
-        matrices.translate(0f, -1.5f, 0f);
-        this.model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySmoothCutout(this.getTexture(entity))), light, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
-        matrices.pop();
+        this.model.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
     }
 
     @Override
--- src/main/java/mc/duzo/persona/client/render/model/P4DoorModel.java
+++ src/main/java/mc/duzo/persona/client/render/model/P4DoorModel.java
@@ -2,10 +2,15 @@ package mc.duzo.persona.client.render.model;
 
 import mc.duzo.persona.common.entity.door.VelvetDoorEntity;
 import mc.duzo.persona.common.entity.door.VelvetDoorVariant;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.model.*;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.model.SinglePartEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.RotationAxis;
 
 public class P4DoorModel extends VelvetRoomDoorModel {
 	private final ModelPart door;
@@ -74,6 +79,26 @@ public class P4DoorModel extends VelvetRoomDoorModel {
 	@Override
 	public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
 		door.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+	}
+
+	@Override
+	public void render(VelvetDoorEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+		matrices.push();
+		matrices.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(180f));
+		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(entity.getBodyYaw()));
+
+		this.getChild("end_portal").ifPresent(cube -> {
+			if(MinecraftClient.getInstance().player.distanceTo(entity) < 2f) {
+				matrices.push();
+				matrices.scale(1f, 1f, 2f);
+				cube.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEndGateway()), 0xFF00F0, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
+				matrices.pop();
+			}
+		});
+
+		matrices.translate(0f, -1.5f, 0f);
+		this.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySmoothCutout(this.getTexture())), light, OverlayTexture.DEFAULT_UV, 1f, 1f, 1f, 1f);
+		matrices.pop();
 	}
 
 	@Override
