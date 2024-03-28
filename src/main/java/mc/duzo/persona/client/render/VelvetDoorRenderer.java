c1c346662f76bbbafa5ac39cf8750afb372cd154
James Hall <james.hall01@outlook.com>
2024-03-28 16:32:56 +0000

    VelvetDoorEntity now looks towards player

--- src/main/java/mc/duzo/persona/client/render/VelvetDoorRenderer.java
+++ src/main/java/mc/duzo/persona/client/render/VelvetDoorRenderer.java
@@ -4,11 +4,14 @@ import mc.duzo.persona.client.render.model.VelvetDoorModel;
 import mc.duzo.persona.client.render.model.VelvetDoorRegistry;
 import mc.duzo.persona.common.entity.door.VelvetDoorEntity;
 import mc.duzo.persona.common.entity.door.VelvetDoorVariant;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.RotationAxis;
 
 import java.util.Optional;
 
@@ -30,7 +33,18 @@ public class VelvetDoorRenderer extends EntityRenderer<VelvetDoorEntity> {
             this.model = found.get();
         }
 
+        matrices.push();
+        // Face towards the player
+
+        if (MinecraftClient.getInstance().player != null) {
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+            matrices.multiply(RotationAxis.NEGATIVE_Y.rotationDegrees(-yaw));
+            matrices.multiply(RotationAxis.NEGATIVE_Y.rotationDegrees(player.getHeadYaw() + 180)); // todo - should face the player, not copy its head
+        }
         this.model.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
+
+        matrices.pop();
     }
 
     @Override
